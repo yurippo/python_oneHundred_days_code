13e8c657b4aec4ac96fe1b2d3820419f71c1e9be
Yuri Prata <yuri.prata12@gmail.com>
2022-11-29 20:24:19 -0800

    Commit 2022-11-29

---
+++ band_name_generator.py
@@ -0,0 +1,16 @@
+#The Band Name generator App generates the name of a band based
+#on the name of the city you grew up in and the name of your pet ;)
+
+print("Welcome to the Band Name Generator App 🎸 ")
+
+city_name = input("What's the name of the city you grew up in?!")
+
+print(city_name)
+
+pets_name = input("What's your pet's name?!")
+
+print(pets_name)
+
+band_name = city_name + pets_name
+
+print("The name of your band is "+ band_name +" 🎸 😎🤘")
